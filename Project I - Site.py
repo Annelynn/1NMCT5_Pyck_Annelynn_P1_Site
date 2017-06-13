9c0b3cb66058b8595c0a2aeabdb9d28e6a532792
Annelynn <annelynn@live.be>
2017-06-14 01:13:23 +0200

    Homepagina, infopagina en accountpagina structuur aangemaakt. Homepagina responsive gemaakt

--- Project I - Site.py
+++ Project I - Site.py
@@ -5,8 +5,16 @@ import os
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
+def home():
     return render_template("home.html")
+
+@app.route('/account')
+def account():
+    return render_template("account.html")
+
+@app.route('/info')
+def info():
+    return render_template('info.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
